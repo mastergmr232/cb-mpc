31cde9f85e684f647303b8a299c7aa921172d7cf
mastergmr232 <108103580+mastergmr232@users.noreply.github.com>
2025-05-19 19:53:48 -0700

    Update openssl.cmake
    refactor(cmake): dynamically link OpenSSL on Linux using find_package
    
    - Updated link_openssl macro to use CMake’s find_package(OpenSSL REQUIRED) for dynamic discovery of OpenSSL libraries and include paths on Linux.
    - Removed hardcoded OpenSSL paths and manual library selection logic.
    - Improved portability and maintainability by relying on CMake’s built-in OpenSSL target (OpenSSL::Crypto).

--- cmake/openssl.cmake
+++ cmake/openssl.cmake
@@ -1,13 +1,7 @@
 macro(link_openssl TARGET_NAME)
   if(IS_LINUX)
-    set(OPENSSL_PATH "/usr/local/opt/openssl@3.2.0")
-    include_directories(${OPENSSL_PATH}/include)
-    target_link_libraries(${TARGET_NAME} PUBLIC ${OPENSSL_PATH}/lib64/libcrypto.a)
+    find_package(OpenSSL REQUIRED)
+    target_include_directories(${TARGET_NAME} PUBLIC ${OPENSSL_INCLUDE_DIR})
+    target_link_libraries(${TARGET_NAME} PUBLIC OpenSSL::Crypto)
   endif()
-
-  if(IS_MACOS)
-    include_directories("/usr/local/opt/openssl@3.2.0/include")
-    target_link_libraries(${TARGET_NAME} PUBLIC
-                          "/usr/local/opt/openssl@3.2.0/lib/libcrypto.a")
-  endif()
-endmacro(link_openssl)
+endmacro()
